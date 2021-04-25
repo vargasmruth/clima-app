4664c2c5aa3c8376775a3d68f43d68fd7f705523
Ruth Vargas <ruth.vargas@srlpunto.com>
2021-04-24 23:30:55 -0400

    feat: add menu

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+const { pausa, inquirerMenu } = require("./helpers/inquirer");
 
-console.log('Hello world!');
+
+const main = async() => {
+    let opt;
+
+    do {
+
+        opt = await inquirerMenu();
+        console.log({opt});
+        
+
+        if( opt !== 0 ) await pausa();
+
+    } while (opt !== 0)
+
+}
+
+main();
